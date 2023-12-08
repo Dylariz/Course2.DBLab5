6faa33169df807404391fe9f0218ae9fdff45338
Dylariz <zarec-k@mail.ru>
2023-12-08 15:40:29 +0300

    Initial commit

---
+++ Course2.DBLab5/DBViewer.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Course2.DBLab5
+{
+    public partial class DBViewer : Form
+    {
+        public DBViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
